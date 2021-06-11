2eeb3ae80b4b153c41c46de005121a7109ce8cb9
jhgx <jhgx@users.noreply.github.com>
2021-06-11 15:12:21 -0400

    Docker build improvements - simplify Dockerfile - copy and build code after installing dependencies - fix to use old pe-parser dependency (current version breaks)

--- Dockerfile
+++ Dockerfile
@@ -5,21 +5,14 @@ RUN apt-get update && apt-get -y upgrade
 RUN apt-get install -y git wget cmake sudo gcc-7 g++-7 python3-pip zlib1g-dev googletest
 RUN apt-get install -y libgtest-dev libgflags-dev libz-dev libelf-dev g++ python3-pip libboost-system-dev libboost-thread-dev libboost-date-time-dev
 
-RUN mkdir /code
+WORKDIR /code/functionsimsearch
+COPY ./build_dependencies.sh .
+RUN ./build_dependencies.sh
 
 # build functionsimsearch
-RUN cd /code && \
-    git clone https://github.com/google/functionsimsearch.git && \
-    cd functionsimsearch && \
-    chmod +x ./build_dependencies.sh && \
-    ./build_dependencies.sh && \
-    make -j 16
+COPY . .
+RUN make -j
 
-# dispatch via entrypoint script
 # recommend mapping the /pwd volume, probably like (for ELF file):
-#
-#    docker run -it --rm -v $(pwd):/pwd functionsimsearch disassemble ELF /pwd/someexe
-VOLUME /pwd
-WORKDIR /code/functionsimsearch
-RUN chmod +x /code/functionsimsearch/entrypoint.sh
+#   docker run -it --rm -v $(pwd):/pwd functionsimsearch disassemble ELF /pwd/someexe
 ENTRYPOINT ["/code/functionsimsearch/entrypoint.sh"]
--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -o errexit
+
 source_dir=$(pwd)
 
 echo "WARNING: You will need GCC 7 or newer, GCC 6 does not implement C++17 yet!"
@@ -21,13 +24,10 @@ cd $source_dir
 mkdir third_party
 cd third_party
 
-# Download Dyninst.
-wget https://github.com/dyninst/dyninst/archive/v9.3.2.tar.gz
-tar xvf ./v9.3.2.tar.gz
-# Download PicoSHA, pe-parse, SPII and the C++ JSON library.
+# Download PicoSHA.
 git clone https://github.com/okdshin/PicoSHA2.git
-git clone https://github.com/trailofbits/pe-parse.git
-git clone https://github.com/PetterS/spii.git
+
+# Build C++ JSON library.
 mkdir json
 mkdir json/src
 cd json/src
@@ -35,13 +35,16 @@ wget https://github.com/nlohmann/json/releases/download/v3.1.2/json.hpp
 cd ../..
 
 # Build PE-Parse.
+git clone https://github.com/trailofbits/pe-parse.git
 cd pe-parse
+git checkout 78869e53372424a1e46ff1996be6a1e136688719  # functionsimsearch doesn't work with the current pe-parse version
 sed -i -e 's/-Wno-strict-overflow/-Wno-strict-overflow -Wno-ignored-qualifiers/g' ./cmake/compilation_flags.cmake
 cmake ./CMakeLists.txt
 make -j
 cd ..
 
 # Build SPII.
+git clone https://github.com/PetterS/spii.git
 cd spii
 cmake ./CMakeLists.txt
 make -j
@@ -49,6 +52,8 @@ sudo make install
 cd ..
 
 # Build Dyninst
+wget https://github.com/dyninst/dyninst/archive/v9.3.2.tar.gz
+tar xvf ./v9.3.2.tar.gz
 cd dyninst-9.3.2
 cmake ./CMakeLists.txt
 # Need to limit parallelism because 8-core DynInst builds exhaust 30 Gigs of
@@ -57,8 +62,3 @@ make -j 4
 sudo make install
 sudo ldconfig
 cd ..
-
-# Finally build functionsimsearch tools
-cd ..
-make -j
-
